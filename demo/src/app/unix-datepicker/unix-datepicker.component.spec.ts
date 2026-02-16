82a01bedc49c7d057ad05ab91d7cbdc930cc0687
admcfarland <amacmusic90@gmail.com>
2026-02-16 00:01:35 -0600

    test(unixdatepicker): more tests
    re #26

--- demo/src/app/unix-datepicker/unix-datepicker.component.spec.ts
+++ demo/src/app/unix-datepicker/unix-datepicker.component.spec.ts
@@ -1,4 +1,3 @@
-
 import { describe, it, expect, beforeEach } from 'vitest';
 import { createComponentFactory, Spectator } from '@ngneat/spectator';
 import { UnixDatepickerComponent } from './unix-datepicker.component';
@@ -12,30 +11,39 @@ const PARSED_VALID_TEN_DIGIT = parseInt(VALID_TEN_DIGIT, 10);
 
 const INVALID_ALPHA_ENTRY = 'abc';
 const INVALID_LENGTH_DIGIT = '123456';
+const INVALID_DIGIT_WITH_SPACES = ' 1771215930 ';
 
-describe('UnixDatepickerComponent with spectator', () => {
+describe('UnixDatepickerComponent creation (spectator)', () => {
   let spectator: Spectator<UnixDatepickerComponent>;
   const createComponent = createComponentFactory({
     component: UnixDatepickerComponent,
     detectChanges: false,
   });
 
-  beforeEach(() => {
-    spectator = createComponent({
-      props: { debounce: DEBOUNCE_TIME_MS }
-    });
-  });
+  beforeEach(() => spectator = createComponent());
 
   it('should create', () => {
     expect(spectator.component).toBeTruthy();
   });
 
-  /* DEFAULT STATES */
   it('should have an empty initial value', () => {
     expect(spectator.component.timestampForm.value.timestamp).toBe('');
   });
 
-  /* ERRORS */
+  it('should have error required on initial construction', () => {
+    expect(spectator.component.errorMessage()).toBe('Timestamp is required');
+  });
+});
+
+describe('UnixDatepickerComponent errors (spectator)', () => {
+  let spectator: Spectator<UnixDatepickerComponent>;
+  const createComponent = createComponentFactory({
+    component: UnixDatepickerComponent,
+    detectChanges: false,
+  });
+
+  beforeEach(() => spectator = createComponent());
+
   it('should show required error when empty and touched', () => {
     const control = spectator.component.timestampForm.controls.timestamp;
     control.markAsTouched();
@@ -62,6 +70,16 @@ describe('UnixDatepickerComponent with spectator', () => {
     const control = spectator.component.timestampForm.controls.timestamp;
 
     control.setValue(INVALID_ALPHA_ENTRY);
+    control.markAsTouched();
+    control.updateValueAndValidity();
+
+    expect(spectator.component.errorMessage()).toMatch('Timestamp must be a number');
+  });
+
+  it('should show pattern error for timestamp with spaces', () => {
+    const control = spectator.component.timestampForm.controls.timestamp;
+
+    control.setValue(INVALID_DIGIT_WITH_SPACES);
     control.markAsTouched();
     control.updateValueAndValidity();
 
@@ -101,7 +119,7 @@ describe('UnixDatepickerComponent with spectator', () => {
   it('should not show error after alpha entry then valid 10-digit timestamp', () => {
     const control = spectator.component.timestampForm.controls.timestamp;
 
-    control.setValue('abc');
+    control.setValue(INVALID_ALPHA_ENTRY);
     control.markAsTouched();
     control.updateValueAndValidity();
 
@@ -110,8 +128,51 @@ describe('UnixDatepickerComponent with spectator', () => {
 
     expect(spectator.component.errorMessage()).toBe('');
   });
+});
 
-  /* OUTPUT EMISSION */
+describe('UnixDatepickerComponent inputs (spectator)', () => {
+  let spectator: Spectator<UnixDatepickerComponent>;
+  const createComponent = createComponentFactory(UnixDatepickerComponent);
+
+  beforeEach(() => spectator = createComponent());
+
+  it('should render the correct placeholder', () => {
+    spectator.setInput('placeholder', 'Test Placeholder');
+    spectator.detectChanges();
+    const input = spectator.query('input[matInput]');
+    expect(input?.getAttribute('placeholder')).toBe('Test Placeholder');
+  });
+
+  it('should render the correct label', () => {
+    spectator.setInput('label', 'Test Label');
+    spectator.detectChanges();
+    const label = spectator.query('mat-label');
+    expect(label?.textContent).toContain('Test Label');
+  });
+
+  // Figure out how to test with MatFormFieldHarness
+  // it('should render the correct appearance', () => {
+  //   spectator.setInput('appearance', 'outline');
+  //   spectator.detectChanges();
+  //   const formField = spectator.query('.mat-mdc-form-field');
+  //   console.log(formField);
+  //   expect(formField?.getAttribute('ng-reflect-appearance')).toBe('outline');
+  // });
+});
+
+describe('UnixDatepickerComponent output (spectator)', () => {
+  let spectator: Spectator<UnixDatepickerComponent>;
+  const createComponent = createComponentFactory({
+    component: UnixDatepickerComponent,
+    // detectChanges: false,
+  });
+
+  beforeEach(() => {
+    spectator = createComponent({
+      props: { debounce: DEBOUNCE_TIME_MS }
+    });
+  });
+
   it('should emit unixDateOutput when valid timestamp is entered', async () => {
     let output;
     const promise = new Promise<Date>(resolve => {
@@ -148,5 +209,16 @@ describe('UnixDatepickerComponent with spectator', () => {
 
     await promise;
     expect(output).toEqual(new Date(PARSED_VALID_TEN_DIGIT));
+  });
+
+  it('should not emit output if input is invalid', async () => {
+    let emitted = false;
+    spectator.output('unixDateOutput').subscribe(() => emitted = true);
+    const control = spectator.component.timestampForm.controls.timestamp;
+    control.setValue('abc');
+    control.markAsTouched();
+    control.updateValueAndValidity();
+    await new Promise(res => setTimeout(res, 10));
+    expect(emitted).toBe(false);
   });
 });
